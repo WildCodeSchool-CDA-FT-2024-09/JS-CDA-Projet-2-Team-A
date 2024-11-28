b3a8356820da7267e933054e14fac2dabb25d914
JEANFRANCOIS HAFID <hafidjeanfrancois@gmail.com>
2024-11-28 14:39:50 +0100

    test

--- client/src/pages/Pageexample.tsx
+++ client/src/pages/Pageexample.tsx
@@ -1,8 +1,9 @@
-export default function Pageexample(props) {
+export default function Pageexample() {
   return (
     <div>
       <h1>toto</h1>
       <h2>tata</h2>
+      <h4>titi</h4>
     </div>
   );
 }
