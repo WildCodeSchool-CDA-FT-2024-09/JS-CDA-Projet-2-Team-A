42572974108af9ddc86588a1109398b5876579d1
JEANFRANCOIS HAFID <hafidjeanfrancois@gmail.com>
2024-11-28 14:31:56 +0100

    testing commit tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
 export default function App() {
   return (
     <>
       <h1>Stock Manage</h1>
+      <Button variant="text">Text</Button>
+      <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+      />
     </>
   );
 }
