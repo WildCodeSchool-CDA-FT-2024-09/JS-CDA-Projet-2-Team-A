4580aefc1878fe3cd6d9272501d9f6077efe3f10
Pas-nou <pascal.renou@live.fr>
2024-12-02 17:18:16 +0100

    US07-Feat/ Adding hard data for viewing the user list

--- client/src/pages/AdminHomePage/AdminHomePage.tsx
+++ client/src/pages/AdminHomePage/AdminHomePage.tsx
@@ -1,10 +1,31 @@
-import UsersList from "../../components/UsersList/UsersList";
+import DashboardList from "../../components/DashboardList/DashboardList";
+
+// TODO : import de fichiers json en attendant d'avoir la connexion à la BDD
+import users from "../../../../server/data/data/mock/users.json";
+import roles from "../../../../server/data/data/mock/roles.json";
 
 export default function AdminHomePage() {
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "name", headerName: "Nom", width: 250 },
+    { field: "role", headerName: "Rôle", width: 250 },
+    { field: "login", headerName: "Login", width: 250 },
+  ];
+
+  const rolesName = new Map(roles.map((role) => [role.id, role.role]));
+
+  const data = users.map((user, index) => ({
+    id: index + 1,
+    name: user.name,
+    role: rolesName.get(user.role),
+    login: user.login,
+  }));
+
+  const title = "Liste des utilisateurs";
+
   return (
     <>
-      <h1>Coucou</h1>
-      <UsersList />
+      <DashboardList title={title} columns={columns} data={data} />
     </>
   );
 }
