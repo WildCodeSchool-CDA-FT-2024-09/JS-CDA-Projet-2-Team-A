f9d2cf3576fe0249fc92549af906682f07f9f403
Pas-nou <pascal.renou@live.fr>
2024-12-03 12:16:50 +0100

    US19-Feat/ Adding the home page after login for the purchasing role

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import AdminHomePage from "./pages/AdminHomePage/AdminHomePage.tsx";
+import InventroyPage from "./pages/InventoryPage/InventoryPage.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
     path: "",
     element: <App />,
     children: [
-      { path: "/achat", element: "buyer", children: [] },
+      { path: "/achat", element: <InventroyPage />, children: [] },
       {
         path: "/approvisionnement",
         element: "supply",
