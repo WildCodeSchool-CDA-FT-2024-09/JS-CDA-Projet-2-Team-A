7175fdb2d49e29f57209ff8bf3ee6a69e561dd5c
Pas-nou <pascal.renou@live.fr>
2024-12-03 17:29:00 +0100

    US06-Feat/ Topbar dynamique on progess

--- client/src/components/TopBar/TopBar.tsx
+++ client/src/components/TopBar/TopBar.tsx
@@ -1,8 +1,36 @@
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import PrecisionManufacturingIcon from "@mui/icons-material/PrecisionManufacturing";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { useUser } from "../../contexts/UserContext";
 
 export default function TopBar() {
+  const { user } = useUser();
+
+  const teamIcon = (role: string) => {
+    switch (role) {
+      case "1": // Achat
+        return <ShoppingCartIcon fontSize="large" sx={{ color: "#383E49" }} />;
+      case "2": // Appro
+        return <AssignmentIcon fontSize="large" sx={{ color: "#383E49" }} />;
+      case "3": // Atelier
+        return (
+          <PrecisionManufacturingIcon
+            fontSize="large"
+            sx={{ color: "#383E49" }}
+          />
+        );
+      case "4": // Admin
+        return (
+          <SupervisorAccountIcon fontSize="large" sx={{ color: "#383E49" }} />
+        );
+    }
+  };
+  console.info(user);
+  console.info(teamIcon);
+
   return (
     <>
       <AppBar
