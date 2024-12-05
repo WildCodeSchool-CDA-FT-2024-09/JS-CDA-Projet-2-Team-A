d9c47712fdc79f0a6302b291dc92e793f74b69d8
JEANFRANCOIS HAFID <hafidjeanfrancois@gmail.com>
2024-12-05 16:33:56 +0100

    fixed index files

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,2 +1,7 @@
+import { NonEmptyArray } from "type-graphql";
 import { ExampleResolver } from "../resolvers/Example";
+
 export { ExampleResolver };
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export const resolvers: NonEmptyArray<Function> = [ExampleResolver];
