0d5c694b39363d781881e8cf9ace3f906bf4a2a5
Pas-nou <pascal.renou@live.fr>
2024-12-11 17:26:49 +0100

    US49-Feat/ summary dashboard in progress

--- client/src/pages/InventoryPage/InventoryPage.tsx
+++ client/src/pages/InventoryPage/InventoryPage.tsx
@@ -1,7 +1,10 @@
 import DashboardList from "../../components/DashboardList/DashboardList";
 import { useAllProductsQuery } from "../../generated/graphql-types";
-import { Box, Typography, Button } from "@mui/material";
-
+import { Box, Typography, Button, Stack, Divider } from "@mui/material";
+import WarningAmberOutlinedIcon from "@mui/icons-material/WarningAmberOutlined";
+import InventoryOutlinedIcon from "@mui/icons-material/InventoryOutlined";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 export default function InventoryPage() {
   const { data, loading, error } = useAllProductsQuery();
 
@@ -64,61 +67,108 @@ export default function InventoryPage() {
 
   if (data)
     return (
-      <Box
-        sx={{
-          borderRadius: "5px",
-        }}
-      >
+      <Box>
         <Box
-          component="section"
           sx={{
             display: "flex",
+            flexDirection: "column",
             justifyContent: "space-between",
-            alignItems: "center",
-            background: "#FFF",
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            borderRadius: "5px 5px 0px 0px",
+            gap: "20px",
           }}
         >
-          <Typography
-            variant="h5"
-            component="h2"
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={2}
             sx={{
-              mt: 3,
-              mb: 3,
-              color: "#383E49",
+              heigth: "50px",
+              background: "#FFF",
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              borderRadius: "5px 5px 0px 0px",
             }}
           >
-            Liste des produits
-          </Typography>
+            <Stack>
+              <Stack direction="row" spacing={2}>
+                <CategoryOutlinedIcon />
+                <Typography>Catégories</Typography>
+              </Stack>
+            </Stack>
+            <Stack>
+              <Stack direction="row" spacing={2}>
+                <InventoryOutlinedIcon />
+                <Typography>Stock global</Typography>
+              </Stack>
+            </Stack>
+            <Stack>
+              <Stack direction="row" spacing={2}>
+                <ExitToAppOutlinedIcon />
+                <Typography>En cours de livraison</Typography>
+              </Stack>
+            </Stack>
+            <Stack>
+              <Stack direction="row" spacing={2}>
+                <WarningAmberOutlinedIcon />
+                <Typography>Alertes</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
           <Box
             sx={{
-              display: "flex",
-              gap: "10px",
+              borderRadius: "5px",
             }}
           >
-            <Button
-              variant="contained"
-              type="submit"
+            <Box
               sx={{
-                height: "40px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                background: "#FFF",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                borderRadius: "5px 5px 0px 0px",
               }}
             >
-              Ajouter un produit
-            </Button>
-            <Button
-              variant="outlined"
-              type="submit"
-              sx={{
-                height: "40px",
-              }}
-            >
-              Modifier un produit
-            </Button>
+              <Typography
+                variant="h5"
+                component="h2"
+                sx={{
+                  mt: 3,
+                  mb: 3,
+                  color: "#383E49",
+                }}
+              >
+                Liste des produits
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "10px",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  type="submit"
+                  sx={{
+                    height: "40px",
+                  }}
+                >
+                  Ajouter un produit
+                </Button>
+                <Button
+                  variant="outlined"
+                  type="submit"
+                  sx={{
+                    height: "40px",
+                  }}
+                >
+                  Modifier un produit
+                </Button>
+              </Box>
+            </Box>
+            <DashboardList columns={columns} data={dataGridProduct} />
           </Box>
         </Box>
-        <DashboardList columns={columns} data={dataGridProduct} />
       </Box>
     );
 }
