2c8e9c4cbdf21933b29236f8d0579c4c84fe1c4f
Pas-nou <pascal.renou@live.fr>
2024-12-11 14:29:28 +0100

    US64-Feat/ Adding graphql types to the supplier entity

--- server/src/entities/supplier.entities.ts
+++ server/src/entities/supplier.entities.ts
@@ -5,44 +5,58 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
+import { ObjectType, Field, Int } from "type-graphql";
 import { Product } from "./product.entities";
 import { Employee } from "./employee.entities";
 
+@ObjectType()
 @Entity("supplier")
 export class Supplier extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field()
   @Column()
   logo: string;
 
+  @Field()
   @Column()
   description: string;
 
+  @Field()
   @Column()
   address: string;
 
+  @Field()
   @Column()
   postcode: string;
 
+  @Field()
   @Column()
   city: string;
 
+  @Field()
   @Column()
   country: string;
 
+  @Field()
   @Column()
   delay: number;
 
+  @Field()
   @Column({ default: true })
   active: boolean;
 
+  @Field()
   @Column({ nullable: true })
   commentary?: string;
 
+  @Field(() => Product)
   @OneToMany(() => Product, (product) => product.supplier)
   products: Product[];
 
