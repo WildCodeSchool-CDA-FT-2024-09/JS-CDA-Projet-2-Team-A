1ca0c053424eeec1f3a84b03b035a918fa070b50
Pas-nou <pascal.renou@live.fr>
2024-12-13 12:22:07 +0100

    US65-Feat/ Config upload filenames

--- uploads/src/routes/upload.routes.ts
+++ uploads/src/routes/upload.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import multer from 'multer';
+import path from "path";
 
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../uploads"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${uniqueSuffix}-${file.originalname}`)
+  },
+});
+
+const upload = multer({ storage });
+
+console.info("je suis upload :", upload)
 
 export const uploadRoutes = router;
