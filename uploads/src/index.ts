03e6efcf0d40b9ac6d4dcc61efaca43f4ae894e9
Pas-nou <pascal.renou@live.fr>
2024-12-13 12:12:46 +0100

    US65-Feat/ Add index file

--- uploads/src/index.ts
+++ uploads/src/index.ts
@@ -1,1 +1,16 @@
-// * Fichier en attente de code
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/upload", uploadRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.info(`🚀  Uploads ready at: http://localhost:${PORT}`)
+})
