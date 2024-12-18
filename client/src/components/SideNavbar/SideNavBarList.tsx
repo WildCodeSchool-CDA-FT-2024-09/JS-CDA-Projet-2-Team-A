1fda8dd1e79def0ab570e2f0304486baa00a3215
scyanth <34488277+scyanth@users.noreply.github.com>
2024-12-18 12:29:29 +0100

    US05-Feat/edit sidenavbar for dynamic link or button

--- client/src/components/SideNavbar/SideNavBarList.tsx
+++ client/src/components/SideNavbar/SideNavBarList.tsx
@@ -5,7 +5,7 @@ import {
   ListItemText,
 } from "@mui/material";
 import { blue } from "@mui/material/colors";
-import { NavLink, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { ReactElement } from "react";
 import { linkType, linkTypeOpt } from "../../types/SideNavBarTypes.ts";
 import Typography from "@mui/material/Typography";
@@ -19,9 +19,20 @@ export default function SideNavBarList({
 }): ReactElement {
   const { pathname } = useLocation();
 
+  const teste = () => {
+    alert("test");
+  };
+
+  let listItemProps = {};
+  if (link.url !== undefined) {
+    listItemProps = { component: "a", href: baseUrl + "/" + link.url };
+  } else {
+    listItemProps = { component: "button", onClick: teste };
+  }
+
   return (
     <ListItem key={link.name}>
-      <ListItemButton>
+      <ListItemButton {...listItemProps}>
         <ListItemIcon
           children={link.icon}
           sx={{
@@ -31,23 +42,7 @@ export default function SideNavBarList({
             color: pathname.includes(`${link.url}`) ? blue[500] : "inherit",
           }}
         ></ListItemIcon>
-        <ListItemText
-          primary={
-            link.url !== undefined ? (
-              <NavLink
-                to={baseUrl + "/" + link.url}
-                style={({ isActive }: { isActive: boolean }) => ({
-                  color: isActive ? blue[500] : "inherit",
-                  textDecoration: "none",
-                })}
-              >
-                {link.name}
-              </NavLink>
-            ) : (
-              <Typography>{link.name}</Typography> // Rajouter ici le onClick/onKeyDown lier à la déconnexion
-            )
-          }
-        />
+        <ListItemText primary={<Typography>{link.name}</Typography>} />
       </ListItemButton>
     </ListItem>
   );
