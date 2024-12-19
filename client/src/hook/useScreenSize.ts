ad220d90c2a1b81c0b465cb180572578f5cf81c5
Pas-nou <pascal.renou@live.fr>
2024-12-19 16:06:42 +0100

    Fix/ Resumption of the useScreenSize hook to put a condition on the presence of the dashboardSummary or not

--- client/src/hook/useScreenSize.ts
+++ client/src/hook/useScreenSize.ts
@@ -1,11 +1,20 @@
 import { useMediaQuery } from "@mui/material";
 
-const useScreenSize = () => {
+const useScreenSize = (options?: { withSummary?: boolean }) => {
   const isSmallScreen = useMediaQuery("(max-width:1920px)");
   const isMediumScreen = useMediaQuery("(max-width:2560px)");
   const isLargeScreen = useMediaQuery("(min-width:2561px)");
 
-  return { isSmallScreen, isMediumScreen, isLargeScreen };
+  const baseHeight = isSmallScreen ? 473 : isMediumScreen ? 734 : 1461;
+
+  const adjustedHeight = options?.withSummary ? baseHeight - 100 : baseHeight;
+
+  return {
+    isSmallScreen,
+    isMediumScreen,
+    isLargeScreen,
+    dataGridHeight: `${adjustedHeight}px`,
+  };
 };
 
 export default useScreenSize;
