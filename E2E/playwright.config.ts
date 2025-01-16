8dc0fe5e7875a124969d18b8c50620df9f8e70f6
Julien RICHARD <julien.y.richard@gmail.com>
2025-01-16 10:35:57 +0100

    Correcting script about predeploy server, migrating the data and correction on playwrigth conf

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    headless: true,
     trace: "on-first-retry",
   },
 
